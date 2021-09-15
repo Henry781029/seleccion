4544ac7213b108feea371087008e606e2c2e96e0
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-09-15 03:22:33 -0500

    se crean metodos y opciones de menu

--- src/convocatoria/Programador.java
+++ src/convocatoria/Programador.java
@@ -11,4 +11,54 @@ public class Programador {
         jugadorDB = new ArrayList<Jugador>();
         inputData = new Scanner(System.in);
     }
+
+    public void agregarJugador(){
+
+    }
+
+    public void buscarJugador(){
+
+    }
+
+    public void editarJugador(){
+
+    }
+
+    public void mostrarTablaJugadores(){
+
+    }
+
+    public void menu(){
+        boolean flag = true;
+
+        while (flag){
+            System.out.println("Seleccione la información requerida del menu");
+            System.out.println("********************************************");
+            System.out.println("1. Agregar Jugador");
+            System.out.println("2. Buscar Jugador");
+            System.out.println("3. Editar Jugador");
+            System.out.println("4. Mostrar Tabla Jugadores");
+            System.out.println("5. Salir");
+
+            int opciones = Integer.parseInt(inputData.nextLine());
+
+            switch (opciones){
+                case 1:
+                    agregarJugador();
+                    break;
+                case 2:
+                    buscarJugador();
+                    break;
+                case 3:
+                    editarJugador();
+                    break;
+                case 4:
+                    mostrarTablaJugadores();
+                    break;
+                case 5:
+                    flag = false;
+                    break;
+            }
+        }
+    }
 }
